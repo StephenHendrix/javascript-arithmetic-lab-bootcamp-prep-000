652c07c3732b374cf9821533457ce151c2b7b8f5
Stephen Hendrix <stephenth12@gmail.com>
2018-01-01 07:39:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function multiply(a,b) {return (a * b)}
 function divide(a,b) {return (a / b)}
 function inc(n) { return (n += 1)}
 function dec(n) { return (n -= 1)}
+
 function makeInt(n) {
-  return
+  return (parseint('n',10))
 }
